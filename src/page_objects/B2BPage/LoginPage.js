ea6d114e3b415ca58ff717ee490b74e872065736
Admin <admin@ThuongNguyens-MacBook-Pro.local>
2025-02-04 20:01:02 +0700

    v4

--- src/page_objects/B2BPage/LoginPage.js
+++ src/page_objects/B2BPage/LoginPage.js
@@ -1,6 +1,8 @@
 const Urls = require('../../../consts/Urls.js')
 const Control = require('../../core/Control.js')
 
+const LOGIN_BUTTON = '//header//div[contains(@class,"account-info")]/a[text()="Login"]'
+const HEADER_LOGIN = '//div[contains(@class,"mx-auto w-full")]/div[contains(@class,"text-xs") and text()="%s"]'
 
 class LoginPage {
 
@@ -8,6 +10,13 @@ class LoginPage {
         await Control._open_url(Urls.LOGIN_URL)
     }
 
+    async clickOnLoginButton() {
+        await Control._click_element(LOGIN_BUTTON)
+    }
+    async isTextLoginOnHeaderDisplayed(text) {
+        await Control._sleep_in_milliseconds(3000)
+        return await Control._is_element_displayed(await Control._generate_dynamic_locator(HEADER_LOGIN, text))
+    }
 }
 
 module.exports = new LoginPage()
--- src/step_definitions/web/b2b_steps/LoginSteps.js
+++ src/step_definitions/web/b2b_steps/LoginSteps.js
@@ -1,12 +1,16 @@
-// const { Given, When, Then } = require('@cucumber/cucumber');
+
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
 const LoginPage = require('../../../page_objects/B2BPage/LoginPage');
+const Assert = require('../../../core/Assert');
+const Control = require('../../../core/Control');
 
 
 // Login Flow
 
 When(/^I login Arobid page$/, async function () {
   await LoginPage.openLoginUrl()
-  
+  await Control._sleep_in_seconds(5)
+  await LoginPage.clickOnLoginButton()
+  await Assert.true(await LoginPage.isTextLoginOnHeaderDisplayed('Login'))
 });
