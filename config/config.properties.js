b871e767fc22fbc2eebbda0178e92a55d897b22c
Admin <admin@ThuongNguyens-MacBook-Pro.local>
2025-02-04 20:12:04 +0700

    v1

--- config/config.properties.js
+++ config/config.properties.js
@@ -22,64 +22,7 @@ const CONFIG = {
 
     // TESTCASE RUN
     features: [
-        './src/specs/**',
-        './src/specs_new/**',
-    ],
-    suites: {
-        sample: ['./src/specs/_sample_feature/**'],
-        e2e: [
-            './src/specs/Smoke/**'
-        ],
-        regression: [
-            './src/specs_new/WebApp/Administration/**',
-            './src/specs_new/WebApp/Clients/**',
-            './src/specs_new/WebApp/GlobalSearch/**',
-            './src/specs_new/WebApp/Configuration/**',
-            './src/specs_new/WebApp/Jobs/**',
-            './src/specs_new/WebApp/MyProfile/**',
-        ],
-        smoke: ['./src/specs/_sample_feature/QA-0001.Login.feature'],
-
-        // Special Suite
-        adhoc: ['./src/specs/Adhoc/**'],
-        admin: ['./src/specs_new/WebApp/Administration/**'],
-        client: ['./src/specs_new/WebApp/Clients/**'],
-        globalSearch: ['./src/specs_new/WebApp/GlobalSearch/**'],
-        config: ['./src/specs_new/WebApp/Configuration/**'],
-        job: ['./src/specs_new/WebApp/Jobs/**'],
-
-        // Sub suite
-        clientM: ['./src/specs_new/WebApp/Clients/ClientsManagement/**'],
-        siteM: ['./src/specs_new/WebApp/Clients/SitesManagement/**'],
-        assetM: ['./src/specs_new/WebApp/Clients/AssetsManagement/**'],
-        contractM: ['./src/specs_new/WebApp/Clients/ContractsManagement/**'],
-        requestM: ['./src/specs_new/WebApp/Clients/RequestsManagement/**'],
-        clientUser: ['./src/specs_new/WebApp/Clients/ClientsUser/**'],
-        clientPortalR: ['./src/specs_new/WebApp/Clients/ClientsPortalRequests/**'],
-        clientPortalU: ['./src/specs_new/WebApp/Clients/ClientsPortalUser/**'],
-        
-        role: ['./src/specs_new/WebApp/Administration/RolesManagement/**'],
-        team: ['./src/specs_new/WebApp/Administration/TeamsManagement/**'],
-        userM: ['./src/specs_new/WebApp/Administration/UsersManagement/**'],
-        
-        checklist: ['./src/specs_new/WebApp/Configuration/Checklists/**'],
-        properties: ['./src/specs_new/WebApp/Configuration/PropertiesConfiguration/**'],
-        rule: ['./src/specs_new/WebApp/Configuration/RuleSettings/**'],
-        workflow: ['./src/specs_new/WebApp/Configuration/Workflows/**'],
-        
-        jobL: ['./src/specs_new/WebApp/Jobs/JobsManagementListView/**'],
-        jobSp: ['./src/specs_new/WebApp/Jobs/JobsManagementSplitView/**'],
-        jobSc: ['./src/specs_new/WebApp/Jobs/JobsScheduler/**'],
-
-        myPf: ['./src/specs_new/WebApp/MyProfile/**'],
-
-        // network log api
-        networklog: ['./src/specs/NetworkLogAPI/*'],
-
-        
-    },
-    exclude: [
-        './src/specs/_sample_feature/KM-8668.feature'
+        './src/specs/**'
     ],
 
     // CREATE TEST EXECUTION REPORT
